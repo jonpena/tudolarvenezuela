3b59353bede1e9edd3b0b9a670fba4d939e149ae
jonpena <gran_200014@hotmail.com>
2023-05-01 10:05:46 -0400

    solving the problem with the data update1

--- src/components/ListCards.tsx
+++ src/components/ListCards.tsx
@@ -11,19 +11,21 @@ const ListCards = () => {
 
   const { data: dolarPrices, isLoading: isLoadingPrices } = useSWR(
     URI_API_AWS,
-    dataFetching
+    dataFetching,
+    { revalidateOnFocus: false, revalidateOnMount: true }
   );
 
   const { data: dolarToday, isLoading: isLoadingDolarToday } = useSWR(
     URI_API_DOLARTODAY,
-    dataFetching
+    dataFetching,
+    { revalidateOnFocus: false, revalidateOnMount: true }
   );
 
   useEffect(() => {
     if (!isLoadingPrices || !isLoadingDolarToday) {
       setPrices(updateData(dolarPrices, dolarToday));
     }
-  }, [isLoadingPrices, isLoadingDolarToday]);
+  }, [dolarPrices, dolarToday]);
 
   return (
     <section className='max-w-7xl mt-20 mx-auto px-14 mb-16 py-8 grid grid-auto-fluid place-items-center  gap-10'>
